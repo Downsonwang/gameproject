c809e8f7bfeac05e32ca9c3ae8346105537a87ea
wangyusong <downsonliteracy@foxmail.com>
2024-04-12 10:31:58 -0400

    init v1/andrew

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Branch v1/wang',
       theme: ThemeData(
         // This is the theme of your application.
         //
