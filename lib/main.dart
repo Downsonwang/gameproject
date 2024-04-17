5f8697331a98396df0542354edeec190a1429aaa
Andrew Ferreira <andrewf289@gmail.com>
2024-04-17 11:26:45 -0400

    added amplify imports to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,12 @@
  * @LastEditTime: 2024-04-12 10:23:13
  */
 import 'package:flutter/material.dart';
+// Amplify Flutter Packages
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+// Generated in previous step
+import 'amplifyconfiguration.dart';
 
 void main() {
   runApp(const MyApp());
