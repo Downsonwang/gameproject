a8e61a11dcb0be2891607c7482e237bd522da312
Andrew Ferreira <andrewf289@gmail.com>
2024-05-04 18:24:07 -0400

    hi

--- my_amplify_app/lib/main.dart
+++ my_amplify_app/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-
 import 'amplifyconfiguration.dart';
+import 'widgets/navigation_bar.dart';
 
 Future<void> main() async {
   try {
@@ -27,22 +27,13 @@ Future<void> _configureAmplify() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Authenticator(
       child: MaterialApp(
         builder: Authenticator.builder(),
-        home: const Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SignOutButton(),
-                Text('TOD Application'),
-              ],
-            ),
-          ),
-        ),
+        home: const NavigationExample(),
       ),
     );
   }
